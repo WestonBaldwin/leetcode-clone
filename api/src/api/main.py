a65178f02fabaffc1066e700f4fe40bfcc99fb9a
Urethane <etjjiao@gmail.com>
2026-09-24 20:39:00 -0400

    added post command

--- api/src/api/main.py
+++ api/src/api/main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class Submission(BaseModel):
+    code: str
 
-@app.get("/")
-def read_root():
-    return {"message": "Hello World"}
+@app.post("/submit-solution")
+def submit_solution(submission: Submission):
+    print(submission.code)
+
+    return {
+        "message": "Solution received",
+        "code": submission.code
+    }
